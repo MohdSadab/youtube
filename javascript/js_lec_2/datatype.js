d7309d035e9cfd1ca58a4282f587c8e85888ca5f
MohdSadab <sadabkhan14198@gmail.com>
2022-06-10 10:49:24 +0530

    js lec 3

--- javascript/js_lec_2/datatype.js
+++ javascript/js_lec_2/datatype.js
@@ -1,128 +1,128 @@
-/**
+ /**
 
-javascript data types
-1. Primitive Datatype     2. Non Primitive
-
-
-Primitive values (immutable datum represented directly at the lowest level of the language).
-
-primitive 
-    . Number
-    . String
-    . Boolean
-    . Undefined
-    . Bigint
-    . Null
-    . Symbol
-
-Non Primitive
-    . Object (an object is a value in memory which is possibly referenced by an identifier.)
+// javascript data types
+// 1. Primitive Datatype     2. Non Primitive
 
 
- */
+// Primitive values (immutable datum represented directly at the lowest level of the language).
 
-    // number 
-    // 1, 1.1 1.111 , -1 ,-0, +0 , +Infinity, -Infinity
+// primitive 
+//     . Number
+//     . String
+//     . Boolean
+//     . Undefined
+//     . Bigint
+//     . Null
+//     . Symbol
+
+// Non Primitive
+//     . Object (an object is a value in memory which is possibly referenced by an identifier.)
+
+
+//  */
+
+//     // number 
+//     // 1, 1.1 1.111 , -1 ,-0, +0 , +Infinity, -Infinity
     
-    // var temp = 102.37887387;
-    // console.log(typeof temp);
+//     // var temp = 102.37887387;
+//     // console.log(typeof temp);
 
-    // using single quote
-    var str =  'dhjdkjjkd';
-    var str =  "dhjdhkd";
+//     // using single quote
+//     var str =  'dhjdkjjkd';
+//     var str =  "dhjdhkd";
 
-    // using template literals
-    var str =  `
-    d;ldl;;ld
-    fplfpof
-    fkfoof
-    fkfooipf`;
+//     // using template literals
+//     var str =  `
+//     d;ldl;;ld
+//     fplfpof
+//     fkfoof
+//     fkfooipf`;
 
-    console.log(typeof str)
-    // console.log(typeof str,str)
-
-
-    //  Boolean data type hold true or false
-
-    // var bool  = true;
-    var bool  = false;
-    console.log(typeof bool, bool);
-
-    var empty;
-
-    console.log(typeof empty,empty);
+//     console.log(typeof str)
+//     // console.log(typeof str,str)
 
 
-    // 2^53-1;
+//     //  Boolean data type hold true or false
 
-    var bigint = 12345n;
+//     // var bool  = true;
+//     var bool  = false;
+//     console.log(typeof bool, bool);
 
-    console.log(typeof bigint, bigint)
+//     var empty;
 
-
-    var i_am_null = null;
-
-    console.log(typeof i_am_null, i_am_null);
-
-    // symbol;
-
-    var symbl = Symbol('key1');
-
-    console.log(typeof symbl,symbl);
+//     console.log(typeof empty,empty);
 
 
-    // non primitive
-    // object key value pair value can be anything (primitive non primitive)  key string (if we don't add string convert in string)
+//     // 2^53-1;
 
-    let obj = {"firstName":"Mohd"};
+//     var bigint = 12345n;
 
-    // second method to add key 
-    obj.second = "Sadab";
-
-    // third method
-    obj['age'] = 23;
-
-    // forth method
-    let key='adress';
-
-    obj[key] = {
-        'street':"djlkd",
-        'city':"Delhi",
-        pincode: 110210,
-        "dkjkd":1
-    }
-
-    console.log(obj,typeof obj);
-
-    // accessing value from object
-
-    console.log(obj.firstName,"firstname", typeof obj.firstName);
-
-    console.log(obj['second'],"lastname", typeof obj['second']);
-
-    console.log(obj[key],key, typeof obj[key]);
-
-    // immutable (can't update the value);
-    // mutable (can update the value)
-
-    // string primitive
-
-    var im_str = 'Immutable';
-
-    // can't update
-    im_str[0]= 'P';
-
-    // can replace
-    im_str = 'DKJHDHKJHKD'
-    console.log(im_str);
+//     console.log(typeof bigint, bigint)
 
 
-    console.log(obj);
+//     var i_am_null = null;
 
-    // can update
-    obj.firstName = 'Hello'
+//     console.log(typeof i_am_null, i_am_null);
 
-    console.log(obj);
+//     // symbol;
+
+//     var symbl = Symbol('key1');
+
+//     console.log(typeof symbl,symbl);
+
+
+//     // non primitive
+//     // object key value pair value can be anything (primitive non primitive)  key string (if we don't add string convert in string)
+
+//     let obj = {"firstName":"Mohd"};
+
+//     // second method to add key 
+//     obj.second = "Sadab";
+
+//     // third method
+//     obj['age'] = 23;
+
+//     // forth method
+//     let key='adress';
+
+//     obj[key] = {
+//         'street':"djlkd",
+//         'city':"Delhi",
+//         pincode: 110210,
+//         "dkjkd":1
+//     }
+
+//     console.log(obj,typeof obj);
+
+//     // accessing value from object
+
+//     console.log(obj.firstName,"firstname", typeof obj.firstName);
+
+//     console.log(obj['second'],"lastname", typeof obj['second']);
+
+//     console.log(obj[key],key, typeof obj[key]);
+
+//     // immutable (can't update the value);
+//     // mutable (can update the value)
+
+//     // string primitive
+
+//     var im_str = 'Immutable';
+
+//     // can't update
+//     im_str[0]= 'P';
+
+//     // can replace
+//     im_str = 'DKJHDHKJHKD'
+//     console.log(im_str);
+
+
+//     console.log(obj);
+
+//     // can update
+//     obj.firstName = 'Hello'
+
+//     console.log(obj);
 
 
 // operators in javascript
@@ -172,11 +172,206 @@ typeof
 returns typeof variable;
 */
 
-let add = 1+2;
 
-let num1 =1
-let num2 =2 
+// + operator addition of two numbers;
 
-add = num1 + num2;
+let num1 =10;
+let num2 =20;
 
-console.log(add)
+let num3 = num1+num2;
+
+console.log(num3);
+
+// + concatenation loperand or roperand  is not a number or boolean 
+
+// num3 = num2 + "10";
+// console.log(num3); // 2010
+
+// num3 =  "10" + num2;
+// console.log(num3);   // 1020
+
+// num3 =  false + num2;
+// console.log(num3);   //20
+
+// num3 =  true + num2;
+// console.log(num3);   //21
+
+// num3 =  null + num2;
+// console.log(num3);   //20
+
+// num3 =  undefined + num2;
+// console.log(num3);   //20
+
+
+
+
+// //  - opertaor
+
+// num3 = num1 - num2;
+// console.log(num3);
+
+// num3 = num2 - num1;
+// console.log(num3);
+
+// num3 = num2 - "";
+// console.log(num3)  //20-0 = 20
+
+// num3 =""- num2 ;
+// console.log(num3)  // 0 - 20 = -20
+
+// num3 = num2 - null;
+// console.log(num3) //20-0 = 20
+
+// num3 = num2 - undefined;
+// console.log(num3) //20-0 = NaN
+
+// num3 = num2 - "sjkdkjd";
+// console.log(num3) //20-0 = NaN
+
+
+// //  - opertaor
+
+// num3 = num1 * num2;
+// console.log(num3);
+
+// num3 = num2 * num1;
+// console.log(num3);
+
+// num3 = num2 * "";
+// console.log(num3)  //20*0 = 0
+
+// num3 =""* num2 ;
+// console.log(num3)  // 0 * 20 = 0
+
+// // num3 = num2 - null;
+// // console.log(num3) //20-0 = 20
+
+// // num3 = num2 - undefined;
+// // console.log(num3) //20-0 = NaN
+
+// // num3 = num2 - "sjkdkjd";
+// // console.log(num3) //20-0 = NaN
+
+// num3 = num2/num1;
+// console.log(num3)
+
+// num3 = num1/num2;
+// console.log(num3)
+
+// num3 = num2/"";
+// console.log(num3) // infinity
+
+// num3 = ""/num2;
+// console.log(num3) // 0
+
+
+// num3 = 30%num2;
+// console.log(num3)
+
+// num3 = num2%num2;
+// console.log(num3)
+
+// num3 = num1%num2;
+// console.log(num3)
+
+// num3 = ""%num2;
+// console.log(num3)
+
+// num3 = 2**4;
+// console.log(num3)
+
+
+// num3 = 2**0;
+// console.log(num3)
+
+// compairision operator returns true or false
+// console.log(num1>num2, `${num1}>${num2}`);
+// console.log(num1<num2, `${num1}<${num2}`);
+
+//  lval operator rval  
+// either lval > rval or lval=rval
+// console.log(3>=3);
+// console.log(3>=4);
+// console.log(3>=2);
+// console.log(3>2>1, `3>2>1`);
+
+// console.log(3<=3);
+// console.log(3<=4);
+// console.log(3<=2);
+// console.log(3<="2");
+
+
+//  == compaires(value) lval to rval  if
+// console.log(1==1);
+// console.log(1==2);
+// console.log(1=='1');
+
+
+//  === compires value and datatype  both are equal true else false
+
+// console.log(1===1);
+// console.log(1===2);
+// console.log(" 1==='1'  ",1==='1',"    1=='1'  ", 1=='1', typeof 1, typeof '1');
+
+// === !== strict compairision
+console.log(1!=1);
+console.log(1!=2);
+console.log(" 1==='1'  ",1!=='1',"    1=='1'  ", 1!='1');
+
+/**
+true && true
+true
+true && false
+false
+true || false
+true
+false || true
+true
+! false
+true
+!true
+false
+ */
+
+// let bool1 = true;
+// let bool2 = [1,3,4,5];
+
+// let bool3 = bool1 && bool2;
+
+// // bool3 have value of bool2(rVal) if bool1(lval) is true
+// // bool3 is false if bool1(lval) is false
+// console.log(bool3)
+
+// let bool1 = false;
+// let bool2 = [1,3,4,5];
+
+// let bool3 = bool1 || bool2;
+
+// // bool3 have value of bool1(rVal) if bool1(lval) is true
+// // bool3 have value of bool2(rval) if bool1(lval) is false
+// console.log(bool3)
+
+num3 = 10;
+num3 +=20;  // num3+=20 => num3=num3+20
+console.log(num3);
+num3 = 10;
+num3 *=20;  // num3*=20 => num3=num3+20
+console.log(num3);
+
+num3 = 10;
+num3 -=20;  // num3-=20 => num3=num3-20
+console.log(num3);
+
+
+num3 = 10;
+num3 %=20;  // num3%=20 => num3=num3%20
+console.log(num3);
+
+num3 = 10;
+num3 /=20;  // num3/=20 => num3=num3/20
+console.log(num3);
+
+
+let greater = 3<4 ? '3 is less then 4 true' : '3 is grater then 4 false';
+
+console.log(greater)
