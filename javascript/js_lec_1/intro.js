f94b5a3aa024e5ab5b41813c8de878c44240a3e0
MohdSadab <sadabkhan14198@gmail.com>
2022-06-10 05:50:53 +0530

    js_lec_1

---
+++ javascript/js_lec_1/intro.js
@@ -0,0 +1,41 @@
+/**
+ 
+JavaScript is a lightweight, cross-platform, and interpreted compiled programming language which is also known as the scripting language for webpages. It is well-known for the development of web pages, many non-browser environments also use it. JavaScript can be used for Client-side developments as well as Server-side developments.
+
+lightweight means Use less cpu.
+platform independent
+
+History of JavaScript: It was created in 1995 by Brendan Eich while he was an engineer at Netscape in 1997 names as es1 (ecmascript).
+
+JavaScript was created in the first place for DOM manipulation. Earlier websites were mostly static, after JS was created dynamic Web sites were made.
+
+
+
+javascript identifiers 
+( the first character must be a letter, an underscore (_), or a dollar sign ($). Subsequent characters may be any letter or digit or an underscore or dollar sign.)
+    1- start with a-z or A-Z or _ and $ only 
+    2- case sensitive a not is equal to A.
+    3- can be declare using var let const keyword;
+
+variables are the containers which holds the value
+
+reserved keywords (can't used as identifiers)
+var, let, const, function, return, if, else, for, while, switch  etc; 
+
+print in javascript
+console.log() // used for print 
+*/
+
+console.log("I am external file")
+
+var _abc=10;
+var $abc =10;
+
+var _Abc=20;
+
+console.log(_abc,_Abc);
+
+
+// static , dynamic
+// dynamic type run time per datatype ka pata chalta hai
+
---
+++ javascript/js_lec_2/datatype.js
@@ -0,0 +1,22 @@
+/**
+
+javascript data types
+1. Primitive Datatype     2. Non Primitive
+js is dynamically typed;
+
+Primitive values (immutable datum represented directly at the lowest level of the language).
+
+primitive 
+    . Number
+    . String
+    . Boolean
+    . Undefined
+    . Bigint
+    . Null
+    . Symbol
+
+Non Primitive
+    . Object (an object is a value in memory which is possibly referenced by an identifier.)
+
+
+ */
